5e35744f54eb6a57ad9760db62d75a5b3571e112
ideallx <shlxzj@gmail.com>
2013-07-06 22:20:57 +0800

    ff

---
+++ Game/heroitem.h
@@ -0,0 +1,33 @@
+#ifndef HEROITEM_H
+#define HEROITEM_H
+
+#include <QtWidgets>
+#include "abstracthero.h"
+
+class heroItem : public QGraphicsItem, public abstractHero
+{
+public:
+    heroItem(const QColor &colork, int lineLength);
+
+    char getSexual() const;
+    char getMoveSphere() const;
+    QString getPlayerName() const;
+    QString getHeroName() const;
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget);
+
+    void mouseMoveEvent(QGraphicsSceneMouseEvent*);
+    void mousePressEvent(QGraphicsSceneMouseEvent *);
+
+private:
+    char sexual;
+    char moveSphere;
+    QString playerName;
+    QString heroName;
+
+    int lineLength;
+    QColor color;
+};
+
+#endif // HEROITEM_H
