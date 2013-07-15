0ce00b4ea6ec599e5a5f8c5b45ea69289f15373c
xiang li <shlxzj@gmail.com>
2013-07-15 22:39:39 +0800

    add tranlate

---
+++ lang__zhCN.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>龙翼编年史</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="69"/>
+        <source>测试项</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="93"/>
+        <source>Qt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <source>游戏视图</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>测试页面</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>测试页面2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>当前回合结束</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="18"/>
+        <source>Test StatusBar Label</source>
+        <translation>状态栏</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="22"/>
+        <source>Red Camp: Player 1</source>
+        <translation>红色阵营： 玩家1</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="26"/>
+        <source>Coordinate: 1, 1</source>
+        <translation>坐标: 1, 1</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="30"/>
+        <source>Hero 1</source>
+        <translation>英雄1</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="38"/>
+        <source>End Turn</source>
+        <translation>结束回合</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="39"/>
+        <source>End Current Turn</source>
+        <translation>结束当前回合</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="70"/>
+        <source>LYBNS</source>
+        <translation>龙翼编年史</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="70"/>
+        <source>loading error</source>
+        <translation>加载失败</translation>
+    </message>
+    <message>
+        <location filename="Widget/mainwindow.cpp" line="197"/>
+        <source>coordinate: </source>
+        <translation>坐标: </translation>
+    </message>
+</context>
+<context>
+    <name>gameMapElement</name>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="30"/>
+        <source>grass</source>
+        <translation>草地</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="34"/>
+        <source>stone</source>
+        <translation>石头</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="39"/>
+        <source>shop</source>
+        <translation>商店</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="44"/>
+        <source>alchemy</source>
+        <translation>炼金阵</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="48"/>
+        <source>spring</source>
+        <translation>泉水</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="52"/>
+        <source>camp</source>
+        <translation>营地</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="56"/>
+        <source>swamp</source>
+        <translation>沼泽</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="60"/>
+        <location filename="Game/gamemapelement.cpp" line="86"/>
+        <source>desert</source>
+        <translation>沙漠</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="64"/>
+        <source>water</source>
+        <translation>深水区</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="68"/>
+        <source>fort</source>
+        <translation>要塞</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="73"/>
+        <source>red camp</source>
+        <translation>红色营地</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="77"/>
+        <source>tree</source>
+        <translation>世界树</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemapelement.cpp" line="82"/>
+        <source>blue camp</source>
+        <translation>蓝色营地</translation>
+    </message>
+</context>
+<context>
+    <name>gameMenu</name>
+    <message>
+        <location filename="Game/gamemenu.cpp" line="12"/>
+        <source>move</source>
+        <translation>移动</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemenu.cpp" line="13"/>
+        <source>attack</source>
+        <translation>攻击</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemenu.cpp" line="14"/>
+        <source>ability</source>
+        <translation>技能</translation>
+    </message>
+    <message>
+        <location filename="Game/gamemenu.cpp" line="15"/>
+        <source>cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>heroItem</name>
+    <message>
+        <location filename="Game/heroitem.cpp" line="8"/>
+        <source>player 1</source>
+        <translation>玩家1</translation>
+    </message>
+    <message>
+        <location filename="Game/heroitem.cpp" line="9"/>
+        <source>hero 1</source>
+        <translation>英雄1</translation>
+    </message>
+</context>
+</TS>
